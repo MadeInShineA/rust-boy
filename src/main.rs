b219861b02941e5fa664a7fdf5cabdc4241724b8
MadeInShineA <olivier.amacker@netplus.ch>
2025-01-11 21:20:46 +0100

    Started working on the CPU

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod cpu;
+
+use cpu::Cpu;
+
 fn main() {
-    println!("Hello, world!");
+    let instructions: Vec<u8> = Vec::from([0b00000001, 0b01000111, 0b10101010, 0b11001110]);
+
+    let cpu: Cpu = Cpu {
+        instructions,
+        ..Cpu::default()
+    };
+
+    cpu.run();
+
+    println!("{cpu:?}")
 }
